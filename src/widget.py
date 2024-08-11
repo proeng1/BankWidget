e919952a3633fe0cdd6e6edf76c2170f37b5a850
Dmitry <process@proengineering.su>
2024-08-11 13:31:24 +0400

    Added new functions: mask_account_card and get_date

---
+++ src/widget.py
@@ -0,0 +1,31 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(info: str) -> str:
+    """
+    Маскирует номер карты или счета.
+
+    :param info: Строка, содержащая тип и номер карты или счета.
+    :return: Строка с замаскированным номером.
+    """
+    if "Счет" in info:
+        account_number = info.split()[-1]
+        masked_account = get_mask_account(int(account_number))
+        return f"{info.split()[0]} {masked_account}"
+    else:
+        card_number = info.split()[-1]
+        masked_card = get_mask_card_number(int(card_number))
+        return f"{' '.join(info.split()[:-1])} {masked_card}"
+
+
+from datetime import datetime
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует строку с датой в формат ДД.ММ.ГГГГ.
+
+    :param date_str: Строка с датой в формате "2024-03-11T02:26:18.671407".
+    :return: Строка с датой в формате "ДД.ММ.ГГГГ".
+    """
+    date_obj = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f")
+    return date_obj.strftime("%d.%m.%Y")
---
+++ tests/test_widget.py
@@ -0,0 +1,22 @@
+import unittest
+from src.widget import mask_account_card, get_date
+
+class TestWidgetFunctions(unittest.TestCase):
+    def test_mask_account_card(self):
+        self.assertEqual(
+            mask_account_card("Visa Platinum 7000792289606361"),
+            "Visa Platinum 7000 79** **** 6361"
+        )
+        self.assertEqual(
+            mask_account_card("Счет 73654108430135874305"),
+            "Счет **4305"
+        )
+
+    def test_get_date(self):
+        self.assertEqual(
+            get_date("2024-03-11T02:26:18.671407"),
+            "11.03.2024"
+        )
+
+if __name__ == "__main__":
+    unittest.main()
