66203b678a882e59bc0ec6ee45c0fe6234ddf4c1
Dmitry <process@proengineering.su>
2024-08-11 13:57:39 +0400

    fixed main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from src.masks import get_mask_account, get_mask_card_number
-
+from src.widget import mask_account_card
 
 def main():
     card_number = input("Enter your card number: ")
     account_number = input("Enter your account number: ")
 
-    masked_card = get_mask_card_number(int(card_number))
-    masked_account = get_mask_account(int(account_number))
+    masked_card = mask_account_card(card_number)
+    masked_account = mask_account_card(account_number)
 
     print(f"Original card number: {card_number}")
     print(f"Masked card number: {masked_card}")
